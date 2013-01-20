94955fb3bdad743dcfba4da794d0ceace0db30f4
michael eschbacher <michaeleschbacher@gmail.com>
2013-01-20 13:39:06 -0600

    Processed default values

--- classes/AJAX.php
+++ classes/AJAX.php
@@ -1,23 +1,44 @@
 <?php
 class AJAX
 {
-	static public function process($funcName, $params)
-	{
-		// grab the general comments for the given function
-		try {
-			$reflection = new ReflectionFunction($funcName);
-			$comments = $reflection->getDocComment();
-		} catch (Exception $e) {
-			throw $e;
-		}
+    static public function process($funcName, $params)
+    {
+        // grab the general comments for the given function
+        try {
+            $reflection = new ReflectionFunction($funcName);
+            $comments = $reflection->getDocComment();
+        } catch (Exception $e) {
+            throw $e;
+        }
 
-		// Grab all the special comments for the funcion
-		preg_match_all('%\s*(\w+)%', $comments, $matches, 2);
-		if (count($matches) == 0) {
-			throw new Exception('No Documentation available');
-		}
+        // Grab all the special comments for the funcion
+        $matches = array();
+        preg_match_all('%\s*(\w+)(?::([\w\'"]+))?%', $comments, $matches, 2);
+        if (count($matches) == 0) {
+            throw new Exception('No Documentation available');
+        }
 
-		$ret = array();
-		return $ret;
-	}
+        $ret = array();
+        
+        // Loop through each line of comments
+        foreach ($matches as $match) {
+            if (!empty($match[2])) {
+                if(empty($params[$match[1]])) {
+                    // hack to handle empty strings
+                    if ($match[2] == '\'\'' || $match[2] == '""') {
+                        $ret[$match[1]] = '';
+                    } else {
+                        // Set the parameter to the default value
+                        $ret[$match[1]] = $match[2];
+                    }
+                } else {
+                    // No default value has been passed
+                    $ret[$match[1]] = $params[$match[1]];
+                }
+            } else {
+                // There is no default set
+            }
+        }
+        return $ret;
+    }
 }
